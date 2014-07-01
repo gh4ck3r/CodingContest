b0e32f1315c22f77edcbf605e0c38ecb70ce3613
Changbin Park <gh4ck3r@gmail.com>
2014-07-01 23:39:10 +0900

    Add common header for testcases

---
+++ include/inputfetcher.h
@@ -0,0 +1,40 @@
+#ifndef __INPUTFETCHER_H__
+#define __INPUTFETCHER_H__
+#include <string>
+#include <sstream>
+class InputFetcher : protected std::istringstream
+{
+  using base = std::istringstream;
+  public:
+    InputFetcher() = delete;
+    explicit InputFetcher(std::istream &is)
+      : mInputStream(is), mAutoNextLine(false) {nextline();}
+
+    InputFetcher& nextline() {
+      std::string linebuf;
+      std::getline(mInputStream, linebuf);
+      base::str(linebuf);
+      base::seekg(0);
+      return *this;
+    }
+    bool autoNextLine()                { return mAutoNextLine; }
+    void autoNextLine(const bool &val) { mAutoNextLine = val; }
+
+    template<typename T>
+    InputFetcher& operator>>(T &rhs) {
+      if(mAutoNextLine && base::eof()) nextline();
+      return static_cast<InputFetcher&>(base::operator>>(rhs));
+    }
+    template<typename T>
+    operator T () {
+      T val;
+      operator>>(val);
+      return val;
+    }
+
+  private:
+    std::istream& mInputStream;
+    bool          mAutoNextLine;
+};
+
+#endif // __INPUTFETCHER_H__
---
+++ include/testcase.h
@@ -0,0 +1,63 @@
+#ifndef __TESTCASE_H__
+#define __TESTCASE_H__
+
+#include <iostream>
+#include <vector>
+
+#include "inputfetcher.h"
+
+class TestCase {
+  public:
+    virtual bool parseInput(InputFetcher &input) = 0;
+    virtual void dump(std::ostream &os) const = 0;
+    virtual bool run() = 0;
+  protected:
+    void printBoundary(std::ostream &os, const char marker) const {
+      char oldfill = os.fill(marker);
+      std::streamsize oldwidth(os.width(80));
+      os << std::string() << std::endl;
+      os.fill(oldfill);
+      os.width(oldwidth);
+    };
+
+};
+
+template <typename T>
+class TestCaseContainer : protected std::vector<T>
+{
+  using base           = std::vector<T>;
+
+  using iterator       = typename base::iterator;
+  using const_iterator = typename base::const_iterator;
+  using size_type      = typename base::size_type;
+
+  public:
+    iterator       begin()       noexcept { return base::begin(); }
+    const_iterator begin() const noexcept { return base::begin(); }
+    iterator       end()         noexcept { return base::end(); }
+    const_iterator end()   const noexcept { return base::end(); }
+    size_type      size()  const noexcept { return base::size(); }
+
+    void addCase(const T &aNewCase) { base::push_back(aNewCase); }
+    void addCase(T &&aNewCase) { base::push_back(aNewCase); }
+
+    bool parseInput(std::istream &is) {
+      InputFetcher input(is);
+
+      int testcase_cnt(input);
+      input.nextline();
+
+      while(testcase_cnt--) {
+        T newcase;
+        if(!newcase.parseInput(input))
+          return false;
+        addCase(newcase);
+      }
+
+      return testcase_cnt==0;
+    }
+  protected:
+  private:
+};
+
+#endif // __TESTCASE_H__
