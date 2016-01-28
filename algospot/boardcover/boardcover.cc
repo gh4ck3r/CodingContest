c18a81fcc24a6ce11fbf6993b131aec130bcbc29
Changbin Park <changbin.park@lge.com>
2016-01-28 16:57:30 +0900

    Implement BOARDCOVER
    https://algospot.com/judge/problem/read/BOARDCOVER#

---
+++ algospot/boardcover/boardcover.cc
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+using Cell = char;
+using Row = vector<Cell>;
+using Board = vector<Row>;
+
+using Coord = pair<int, int>;
+using Shape = array<Coord, 3>;
+using Shapes = array<Shape, 4>;
+
+const Cell black('#');
+const Cell empty('.');
+const Cell block('%');
+
+size_t C, H, W;
+Board board;
+Shapes shapes({
+  Shape({Coord(0, 0), Coord(0, 1), Coord(1, 0)}),
+  Shape({Coord(0, 0), Coord(0, 1), Coord(1, 1)}),
+  Shape({Coord(0, 0), Coord(1, 0), Coord(1, 1)}),
+  Shape({Coord(0, 0), Coord(1, 0), Coord(1, -1)})
+});
+
+inline bool available(const size_t row, const size_t col, const Shape &shape)
+{
+  for (const auto &cell : shape){
+    if (board[row + cell.first][col + cell.second] != empty) return false;
+  }
+  return true;
+}
+
+inline bool put(const size_t row, const size_t col, const Shape &shape)
+{
+  if (!available(row, col, shape)) return false;
+  for (const auto &cell : shape) {
+    board[row+cell.first][col+cell.second] = block;
+  }
+  return true;
+}
+
+inline void remove(const size_t row, const size_t col, const Shape &shape)
+{
+  for (const auto &cell : shape) {
+    board[row+cell.first][col+cell.second] = empty;
+  }
+}
+
+size_t count(const size_t row = 0, const size_t col = 0)
+{
+  if (row == H - 1) {
+    for (auto &cell : board[row]) if (cell == empty) return 0;
+    return 1;
+  }
+  if (col == W) return count(row+1, 0);
+  if (board[row][col] != empty) return count(row, col+1);
+
+  size_t cnt=0;
+  for (const auto &shape : shapes) {
+    if (put(row, col, shape)) {
+      cnt += count(row, col+1);
+      remove(row, col, shape);
+    }
+  }
+  return cnt;
+}
+
+int main()
+{
+  cin >> C;
+  while(C--) {
+    cin >> H >> W;
+
+    board.assign(H, Row(W));
+    auto emptyCnt = 0;
+    for (auto &row : board) {
+      for (auto &cell: row) {
+        cin >> cell;
+        if (cell == empty) ++emptyCnt %= 3;
+      }
+    }
+    cout << (emptyCnt ? 0 : count()) << endl;
+  }
+  return 0;
+}
