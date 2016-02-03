7d3a017f97d7a32cf13eecdd3268cfce5641568c
Changbin Park <changbin.park@lge.com>
2016-02-03 14:55:17 +0900

    [Algospot][NUMBERGAME] Apply memoization -- pass
    https://algospot.com/judge/problem/read/NUMBERGAME

--- algospot/numbergame/numbergame.cc
+++ algospot/numbergame/numbergame.cc
@@ -2,29 +2,35 @@
 #include <vector>
 #include <algorithm>
 #include <array>
+#include <map>
 
 using namespace std;
 
 using GameBoard = vector<int>;
+using Key = pair<int, int>;
+map<Key, int> Cache;
 
-int play(const GameBoard &board)
+int play(const GameBoard &board, const int &begin, const int &len)
 {
-  const auto &left(board.front()), &right(board.back());
+  Key key(begin, len);
+  if (Cache[key]) return Cache[key];
 
-  switch (board.size()) {
-    case 0: return 0;
-    case 1: return left;
-    case 2: return (left > right ? 1 : -1) * (left - right);
+  const auto &left(board[begin]), &right(board[begin + len - 1]);
+
+  switch (len) {
+    case 0: return Cache[key] = 0;
+    case 1: return Cache[key] = left;
+    case 2: return Cache[key] = (left > right ? 1 : -1) * (left - right);
   }
 
   array<int, 4> values {
-    left  - play(GameBoard(board.begin()+1, board.end()  )),
-    right - play(GameBoard(board.begin()  , board.end()-1)),
-          - play(GameBoard(board.begin()+2, board.end()  )),
-          - play(GameBoard(board.begin()  , board.end()-2))
+    left  - play(board, begin+1, len-1),
+    right - play(board, begin  , len-1),
+          - play(board, begin+2, len-2),
+          - play(board, begin  , len-2)
   };
 
-  return *max_element(values.begin(), values.end());
+  return Cache[key] = *max_element(values.begin(), values.end());
 }
 
 int main()
@@ -34,9 +40,11 @@ int main()
   while (C--) {
     cin >> n;
 
+    Cache.clear();
     GameBoard board(n);
     for (auto &cell : board) cin >> cell;
-    cout << play(board) << endl;
+
+    cout << play(board, 0, board.size()) << endl;
   }
   return 0;
 }
