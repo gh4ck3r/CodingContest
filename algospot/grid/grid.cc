bd42fdc3e9a7984331e7e380cfdaadb645b6fa8b
Changbin Park <gh4ck3r@gmail.com>
2016-02-04 21:47:43 +0900

    [Algospot][GRID] Optimize recursive version
    https://algospot.com/judge/problem/read/GRID

--- algospot/grid/grid.cc
+++ algospot/grid/grid.cc
@@ -1,61 +1,36 @@
 #include <iostream>
-#include <array>
-#include <vector>
 
 using namespace std;
 
-using Cell = int;
-using GridColumn = array<Cell, 4>;
+enum Shape {shape0, shape1, shape2};
 
-array<GridColumn, 5> ValidColumns {
-  GridColumn
-  {0, 0, 0, 0},
-  {1, 1, 0, 0},
-  {1, 0, 0, 1},
-  {0, 0, 1, 1},
-  {1, 1, 1, 1}
-};
-GridColumn invalid1{0, 1, 1, 0};
-
-int nWaysToTile(int width, GridColumn first_col = ValidColumns[0])
+size_t nWaysToTile(size_t width, Shape first_col = shape0)
 {
-  switch (width) {
-    case 0: return 0;
-    case 1:
-      for (const auto &valid : ValidColumns) {
-        if (first_col == valid) return 1;
-      }
-      return 0;
+  switch (width--) {
+    case 0: if (first_col != shape0) return 0;
+    case 1: return 1;
   }
 
-  int cnt = 0;
-  if (first_col == ValidColumns[0]) {
-    cnt += nWaysToTile(width-1, ValidColumns[0]);
-    cnt += nWaysToTile(width-1, ValidColumns[1]);
-    cnt += nWaysToTile(width-1, ValidColumns[2]);
-    cnt += nWaysToTile(width-1, ValidColumns[3]);
-    cnt += nWaysToTile(width-1, ValidColumns[4]);
-  } else if (first_col == ValidColumns[1]) {
-    cnt += nWaysToTile(width-1, ValidColumns[0]);
-    cnt += nWaysToTile(width-1, ValidColumns[3]);
-  } else if (first_col == ValidColumns[2]) {
-    cnt += nWaysToTile(width-1, ValidColumns[0]);
-    cnt += nWaysToTile(width-1, invalid1);
-  } else if (first_col == ValidColumns[3]) {
-    cnt += nWaysToTile(width-1, ValidColumns[0]);
-    cnt += nWaysToTile(width-1, ValidColumns[1]);
-  } else if (first_col == ValidColumns[4]) {
-    cnt += nWaysToTile(width-1, ValidColumns[0]);
-  } else if (first_col == invalid1) {
-    cnt += nWaysToTile(width-1, ValidColumns[2]);
+  size_t cnt = nWaysToTile(width, shape0);
+  switch (first_col) {
+    case shape0:
+      cnt += nWaysToTile(width, shape1) * 2
+           + nWaysToTile(width, shape2)
+           + nWaysToTile(width-1, shape0);
+      break;
+    case shape1:
+      cnt += nWaysToTile(width, shape1);
+      break;
+    case shape2:
+      cnt += nWaysToTile(width-1, shape2);
+      break;
   }
-
   return cnt;
 }
 
 int main()
 {
-  int N, n = 0, W;
+  size_t N, n = 0, W;
   cin >> N;
   while (N--) {
     cin >> W;
