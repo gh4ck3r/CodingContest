cc1795050a1b065cb108bf67ec0137c294a62b04
Changbin Park <changbin.park@lge.com>
2016-02-17 13:46:24 +0900

    [Algospot][ROUTING] Solved with priority_queue.
    https://algospot.com/judge/problem/read/ROUTING

--- algospot/routing/routing.cc
+++ algospot/routing/routing.cc
@@ -1,76 +1,86 @@
 #include <iostream>
 #include <iomanip>
+#include <utility>
+#include <map>
+#include <vector>
 #include <limits>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
-using Node = size_t;
-using NoiseRatio = double;
+using Noise      = double;
+using Computer   = size_t;
+using NoiseInfo  = map<Computer, Noise>;
+using NoiseMap   = vector<NoiseInfo>;
 
-const Node MaxNode = 10000;
-const NoiseRatio MaxNoiseRatio = numeric_limits<NoiseRatio>::max();
+using NoiseInfoQEntry = pair<NoiseInfo::mapped_type, NoiseInfo::key_type>;
+using NoiseInfoQ      = priority_queue<NoiseInfoQEntry,
+                                       vector<NoiseInfoQEntry>,
+                                       greater<NoiseInfoQEntry>>;
 
-size_t C, N, M;
-NoiseRatio noiseBetween[MaxNode][MaxNode];
-
-NoiseRatio minNoiseRatio()
+istream &operator>>(istream &is, NoiseMap &aNoiseMap)
 {
-  const Node src(0), dst(N-1);
+  Noise c;
+  Computer a, b;
+  is >> a >> b >> c;
 
-  NoiseRatio noiseTo[N];
-  bool networkIncludes[N];
+  aNoiseMap[a][b] = c;
+  aNoiseMap[b][a] = c;
 
-  for (auto n(src); n < N; ++n) {
-    noiseTo[n] = MaxNoiseRatio;
-    networkIncludes[n] = false;
+  return is;
+}
+
+Noise minNoiseRatio(const NoiseMap &aNoiseMap)
+{
+  const Computer src(0), dst(aNoiseMap.size() - 1);
+  const Noise &infinite_noise(numeric_limits<Noise>::infinity());
+
+  NoiseInfo cost_to;
+  NoiseInfoQ nodes_to_enclose;
+
+  nodes_to_enclose.push({cost_to[src] = 1, src});
+  for (auto node(src+1); node <= dst; ++node) {
+    nodes_to_enclose.push({cost_to[node] = infinite_noise, node});
   }
 
-  noiseTo[src] = 1;
-  for (size_t cnt(src); cnt < N; ++cnt) {
-    Node shortestNode;
-    NoiseRatio minNoise = MaxNoiseRatio;
-    for (Node n(src); n < N; ++n) {
-      if (networkIncludes[n]) continue;
-      const auto &noise(noiseTo[n]);
-      if (minNoise > noise) {
-        minNoise = noise;
-        shortestNode = n;
-      }
-    }
-    networkIncludes[shortestNode] = true;
+  while (!nodes_to_enclose.empty()) {
+    const auto &shortest_node_info(nodes_to_enclose.top());
+    const auto queued_cost(shortest_node_info.first);
+    const auto shortest_node(shortest_node_info.second);
+    nodes_to_enclose.pop();
 
-    for (Node n(src); n < N; ++n) {
-      if (noiseBetween[shortestNode][n] == MaxNoiseRatio) continue;
-      if (noiseTo[n] > noiseTo[shortestNode] * noiseBetween[shortestNode][n]) {
-        noiseTo[n] = noiseTo[shortestNode] * noiseBetween[shortestNode][n];
+    const auto cost_to_shortest = cost_to[shortest_node];
+    if (queued_cost > cost_to_shortest) continue;
+
+    for (const auto &adj_info : aNoiseMap[shortest_node]) {
+      const auto &adj(adj_info.first);
+      auto cost_to_adj(cost_to_shortest * adj_info.second);
+      auto &known_cost_to_adj(cost_to[adj]);
+      if (cost_to_adj < known_cost_to_adj) {
+        known_cost_to_adj = cost_to_adj;
+        nodes_to_enclose.push({cost_to_adj, adj});
       }
     }
   }
 
-  return noiseTo[dst];
+  return cost_to[dst];
 }
 
 int main()
 {
   cout << fixed << setprecision(10);
+
+  size_t C, N, M;
   cin >> C;
   while (C--) {
     cin >> N >> M;
 
-    fill(&noiseBetween[0][0], &noiseBetween[N-1][N], MaxNoiseRatio);
-    for (size_t  m(0); m < M; ++m) {
-      size_t a, b;
-      NoiseRatio c;
-      cin >> a >> b >> c;
+    NoiseMap noise_map(N);
+    while (M--) cin >> noise_map;
 
-      auto &knownNoise(noiseBetween[a][b]);
-      if (knownNoise > c) {
-        knownNoise = noiseBetween[b][a] = c;
-      }
-    }
-
-    cout << minNoiseRatio() << endl;
+    cout << minNoiseRatio(noise_map) << endl;
   }
+
   return 0;
 }
