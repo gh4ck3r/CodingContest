91ac3cd456bae3d3cf9172fa996a7f12ae9c432d
Changbin Park <changbin.park@lge.com>
2016-02-18 14:10:39 +0900

    [Algospot][STRJOIN] Add skeleton
    https://algospot.com/judge/problem/read/STRJOIN

---
+++ algospot/strjoin/strjoin.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  size_t c, n;
+  cin >> c;
+  while (c--) {
+    cin >> n;
+
+    vector<int> lengths(n);
+    for (auto &l : lengths) cin >> l;
+
+  }
+  return 0;
+}
