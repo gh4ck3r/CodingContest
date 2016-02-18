64d14dcd3fe9ee5ad216492e7e87f236a1a2326b
Changbin Park <gh4ck3r@gmail.com>
2016-02-18 22:56:45 +0900

    [Algospot][LUNCHBOX] solved
    https://algospot.com/judge/problem/read/LUNCHBOX

--- algospot/lunchbox/lunchbox.cc
+++ algospot/lunchbox/lunchbox.cc
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <utility>
+#include <algorithm>
 
 using namespace std;
+
+struct Lunch : public pair<int&, int&> {
+  Lunch() : pair(e, m){}
+  int m, e;
+};
 
 int main()
 {
@@ -10,9 +17,22 @@ int main()
   while (T--) {
     cin >> N;
 
-    vector<int> M(N), E(N);
-    for (auto &m : M) cin >> m;
-    for (auto &e : E) cin >> e;
+    vector<Lunch> lunches(N);
+    for (auto &l : lunches) cin >> l.m;
+    for (auto &l : lunches) cin >> l.e;
+
+    sort(lunches.begin(), lunches.end(), [](const Lunch &l1, const Lunch &l2) {
+		return l1.e > l2.e;
+	});
+
+    size_t m_time(0), e_time(0);
+    for (const auto &l : lunches) {
+      m_time += l.m;
+      e_time = max(e_time, m_time + l.e);
+    }
+
+    cout << e_time << endl;
   }
+
   return 0;
 }
